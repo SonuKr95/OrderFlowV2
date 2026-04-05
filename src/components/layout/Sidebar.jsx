92fa27656b734882c2d0ffbcd95d510c42afcf63
Sonu Kumar <61611771+SonuKr95@users.noreply.github.com>
2026-04-05 17:52:55 +0530

    updated slightly

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -19,10 +19,14 @@ export default function Sidebar({ collapsed, setCollapsed }) {
 
       <SidebarMenu collapsed={collapsed} />
       {!collapsed && (
-        <p className="text-text-primary">
+        <p className="text-text-secondary mt-3.5 text-center">
           Developed and Managed By :{" "}
-          <span className="decoration-10px text-blue-500 underline">
-            <a target="_blank" href="https://www.facebook.com/sonukr095">
+          <span className="text-text-primary font-semibold underline underline-offset-2">
+            <a
+              target="_blank"
+              href="https://www.facebook.com/sonukr095"
+              title="Contact Administrator  "
+            >
               Sonu Kumar
             </a>
           </span>
--- src/features/auth/api/anonymousUserLogin.js
+++ src/features/auth/api/anonymousUserLogin.js
@@ -3,6 +3,5 @@ import supabase from "../../../services/supabase";
 export async function anonymousUserLogin() {
   const { data, error } = await supabase.auth.signInAnonymously();
   if (error) throw error;
-  console.log(data);
   return data;
 }
